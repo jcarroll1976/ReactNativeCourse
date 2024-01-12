23fda8365dcdb4a7ccfb73c5de0ab80096d8b4f4
Joshua Carroll <carrolljosh1976@gmail.com>
2024-01-11 20:13:24 -0500

    implemented flexbox for styling on text input and goals button

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View,Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>Another piece of text!</Text>
+    <View style = {styles.appContainer}>
+      <View style = {styles.inputContainer}>
+        <TextInput style = {styles.textInput} placeholder='Your course goal!'/>
+        <Button title='Add Goal'/>
       </View>
-      <Text style={styles.helloText}>Hello World!</Text>
-      <Button title = "Tap me!" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  helloText: {
-    margin: 16,
-    borderWidth: 1,
-    borderColor: "red", 
-    padding: 16,
-  }
+ appContainer: {
+  padding: 50,
+ },
+ inputContainer: {
+  flexDirection: "row",
+  justifyContent: "space-between",
+ },
+ textInput: {
+  borderWidth: 1,
+  borderColor: "#cccccc",
+ }
 });
