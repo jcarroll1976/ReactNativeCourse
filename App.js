8879d0442e47c7cd0cb56de6a0d262e4c262f9ed
Joshua Carroll <carrolljosh1976@gmail.com>
2024-01-13 22:41:14 -0500

    Added GoalInput and GoalItem components

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View,Button, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View,Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoalText,setEnteredGoalText] = useState("");
@@ -10,7 +10,7 @@ export default function App() {
   };
 
   function addGoalHandler() {
-    setCourseGoals(currentCourseGoals => [...currentCourseGoals,enteredGoalText]);
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals,{text: enteredGoalText, id: Math.random().toString()}]);
   };
 
   return (
@@ -23,13 +23,20 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
       <View style = {styles.goalsContainer}>
-        <ScrollView>
-          {courseGoals.map((goal) => 
-            <View style = {styles.goalItem} key={goal}>
-              <Text style = {styles.goalText}>{goal}</Text>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style = {styles.goalItem}>
+              <Text style = {styles.goalText}>{itemData.item.text}</Text>
             </View>
-          )}
-        </ScrollView>
+            );
+          }}
+          keyExtractor={(item,index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );
